d2a1e1faf617697734ce5d2081f44185b43f822a
Josue Baez <josuejbaeze4gmail.com>
2025-02-07 18:31:33 -0400

    first deployment

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import { Server as WebSocketServer } from "socket.io";
 import http from "http";
 import { connectDB } from './db'
 import socket  from "./socket";
+import { PORT } from './config';
 
 connectDB();
 const server = http.createServer(app);
-const httpServer = server.listen(3000);
-console.log('Listening on port 3000');
+const httpServer = server.listen(PORT);
+console.log('Listening on port', PORT);
 
 const io = new WebSocketServer(httpServer);
 socket(io);
